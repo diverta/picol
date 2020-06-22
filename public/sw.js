7ca5f2125b28fa6ef2fc911ffff4b76d58b276e8
KentaKatoh <kenta@diverta.co.jp>
2020-06-22 14:59:30 +0900

    Change cache logic.

--- public/sw.js
+++ public/sw.js
@@ -1,12 +1,12 @@
 self.addEventListener('fetch', e => {
-  const cache_url = e.request.url.substring(0,100);
+  const cache_url = e.request.url.substring(0,e.request.url.indexOf("GoogleAccessId"));
 
-  if (cache_url.indexOf('storage.googleapis.com/picol-c8553.appspot.com') == -1) {
+  if (cache_url.indexOf('storage.googleapis.com') == -1) {
     return;
   }
 
   // For example https://storage.googleapis.com/picol-c8553.appspot.com/files/topics/809_ext_04_0.jpg?GoogleAccessId=
-  const cache_nm = 'picol-c8553.appspot.com';
+  const cache_nm = 'picol-storage.googleapis.com';
 
   e.respondWith(
     caches.open(cache_nm).then(function (cache) {
