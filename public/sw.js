cdba0b520a440a90ac0415b1308bfc02b0219c1c
KentaKatoh <kenta@diverta.co.jp>
2020-07-06 01:58:17 +0900

    bugfix

--- public/sw.js
+++ public/sw.js
@@ -1,18 +1,18 @@
 self.addEventListener('fetch', e => {
-  const cache_url = e.request.url.substring(0,e.request.url.indexOf("GoogleAccessId"));
+  const cache_url = e.request.url.substring(0,e.request.url.indexOf("GoogleAccessId")-1);
 
   if (cache_url.indexOf('storage.googleapis.com') == -1) {
     return;
   }
 
   // For example https://storage.googleapis.com/picol-c8553.appspot.com/files/topics/809_ext_04_0.jpg?GoogleAccessId=
-  const cache_nm = 'picol-storage.googleapis.com';
+  const cache_nm = 'storage.googleapis.com';
 
   e.respondWith(
     caches.open(cache_nm).then(function (cache) {
       return cache.match(cache_url).then(function (response) {
         if (response) {
-          console.log(cache_url);
+          //console.log(cache_url);
           return response;
         } else {
           return fetch(e.request.clone()).then(function (response) {
