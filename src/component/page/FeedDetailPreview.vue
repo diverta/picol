990b23383265e0bd421de9076642f982c754f9b9
YuseiAbe <yabe@diverta.co.jp>
2021-07-02 17:07:16 +0900

    feat: add logger reporter

--- src/component/page/FeedDetailPreview.vue
+++ src/component/page/FeedDetailPreview.vue
@@ -12,17 +12,10 @@
 
 <script lang="ts">
 import FeedContainer from '@/component/view/feed/FeedContainer.vue';
-import { UserStateModule, TagStateModule } from '@/store';
-import { FeedStateModule } from '@/store/feed';
-import { FeedModel, CommentModel } from '@/type/api';
-import { CreateElement, VNode } from 'vue';
-import { Vue, Prop } from 'vue-property-decorator';
-import Component from 'vue-class-component';
-import { Route } from 'vue-router';
-import { ServiceHelper } from '@/util';
 import { CONSTANTS } from '@/core';
-import { abortLoadingBigDatas } from '../atom/RouterLinkExt.vue';
-import { ContentService } from '@/kuroco_api';
+import { ContentService, LocalStorage } from '@/kuroco_api';
+import Component from 'vue-class-component';
+import { Vue } from 'vue-property-decorator';
 
 @Component<FeedDetail>({
   components: {
@@ -54,6 +47,10 @@ export default class FeedDetail extends Vue {
   // LIFECYCLE HOOKS
   async mounted() {
     try {
+      const companyCode = this.$route.query.company_cd as string;
+      if (companyCode !== undefined) {
+        LocalStorage.setCompanyCd(companyCode);
+      }
       const previewToken = this.$route.query.preview_token as string;
       const feed = (await ContentService.getContentServiceRcmsApi1Preview({ previewToken })).body;
       feed.details.inst_ymdhi = new Date().toUTCString();
